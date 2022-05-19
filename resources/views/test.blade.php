005affb9d01fcf633fa6d65bf2962f27b77d59c7
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2022-05-19 19:37:31 +0200

    commit on views

---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ma page test</title>
+  </head>
+  <body>
+      @include('navbar')
+    <p>Test</p>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
+});
+
+Route::get('/test', function(){
+    return view('test');
+});
+
+Route::get('/view', function(){
+    return view('folder.view');
 });
