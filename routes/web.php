afc98a058dac953b2a1074bf18968b635c522736
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2022-05-26 12:03:59 +0200

    CommitoftheD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,5 @@ Route::get('/', function(){
     return view('welcome');
 });
 
-Route::get('/test', function(){
-    return view('test');
-});
+Route::resource('articles', ArticleController::class);
+
