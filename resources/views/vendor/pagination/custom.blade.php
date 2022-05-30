5c38947e821bfdafcccc08f99276e39db255c0d6
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2022-05-30 18:02:09 +0200

    PaginationButton

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -1,28 +1,33 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 @if ($paginator->hasPages())
     <ul class="pagination">
         
         @if ($paginator -> onFirstPage())
-            <li class="page-item disabled"><span class="page-link"><i class="fas fa arrow-left"></i></span></li>
+            <li class="page-item disabled"><span class="page-link"><i class="fa-solid fa-angle-left"></i></span></li>
         @else
-            <li class="page-item"><a class="page-link" href="{{ $paginator -> previousPageUrl() }}" rel="prev"><i class="fas fa arrow-left"></i></a></li>
-            @endif
+            <li class="page-item"><a class="page-link" href="{{ $paginator -> previousPageUrl() }}" rel="prev"><i class="fa-solid fa-angle-left"></i></a></li>
+        @endif
 
     @foreach ($elements as $element)
         @if (is_string($element))
             <li class="page-item disabled"> <span class="page-link"> {{ $element }}</span></li>
-            @endif
+        @endif
 
             @if (is_array($element))
                 @foreach ($element as $page => $url)
-                @if ($page == $paginator -> currentPage())
-                <li class="page-item active my-active"><span class = "page-link">{{ $page }}</span></li>
-                @else
-                <li class="page-item"><a class = "page-link" href= "{{ $url }}">{{ $page }}</a></li>
-                    
-                @endif
+                    @if ($page == $paginator -> currentPage())
+                        <li class="page-item active my-active"><span class = "page-link">{{ $page }}</span></li>
+                    @else
+                        <li class="page-item"><a class = "page-link" href= "{{ $url }}">{{ $page }}</a></li>
+                    @endif
                 @endforeach
             @endif
         @endforeach
+        @if ($paginator -> onLastPage())
+        <li class="page-item disabled"><span class="page-link"><i class="fa-solid fa-angle-right"></i></span></li>
+    @else
+        <li class="page-item"><a class="page-link" href="{{ $paginator -> nextPageUrl() }}" rel="prev"><i class="fa-solid fa-angle-right"></i></a></li>
+    @endif
 
     </ul>
 @endif
