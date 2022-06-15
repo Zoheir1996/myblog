617c63ef04773ac91b3b7c0cb99fd8afdb3d1cde
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2022-06-15 02:53:42 +0200

    Personnalisation du model et creation d'un nouvel article

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,6 +3,9 @@
 @section('content')
   <div class="container">
     <h1 class="text-center my-5">Articles</h1>
+    <div class="d-flex justify-content-center">
+      <a class="btn btn-info my-3" href="{{ route('articles.create')  }}"><i class = 'fas fa-plus mx-2'></i>Ajouter un nouvel article</a>
+    </div>
     <table class="table table-hover">
       <thead>
         <tr class="table-active">
@@ -18,7 +21,7 @@
           <tr>
             <th>{{ $article->id  }}</th>
             <td>{{ $article->title  }}</td>
-            <td>{{ $article->created_at  }}</td>
+            <td>{{ $article->dateFormatted() }}</td>
             <td class="d-flex">
               <a href="#" class ="btn btn-warning mx-3">Editer</a>
               <a href="#" class ="btn btn-danger mx-3">Supprimer</a>
