ad9e1deeac081821f658c05732ebd4c38338fc3b
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2022-06-15 03:36:44 +0200

    Integration de l'editeur de texte

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -13,7 +13,7 @@
 </div>
 <div class="container">
     <p class="text-center">
-        {{$article->content}}
+        {!!$article->content!!}
     </p>
 </div>
 
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{  asset('css/bootstrap.min.css')  }}">
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>s
 
     <title>Hello, world!</title>
   </head>
@@ -14,6 +15,7 @@
     @include('incs.navbar')
 
     @yield('content')
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script src = "https://kit.fontawesome.com/907683f3ba.js" ></script>
