b8875b12d218f0d498bb1b38bbcc370acb135877
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2022-06-20 15:55:07 +0200

    Creation du manager

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,12 @@ Route::get('/articles{article:slug}', [MainController::class,'show']) ->name('ar
 
 Auth::routes();
 
-Route::get('/admin/articles', [ArticleController::class, 'index'])->middleware('admin')->name('articles.index');
-Route::get('/admin/articles/create', [ArticleController::class, 'create'])->middleware('admin')->name('articles.create');
-Route::post('/admin/articles/store', [ArticleController::class, 'store'])->middleware('admin')->name('articles.store');
-Route::delete('/admin/articles/{article}/delete}', [ArticleController::class, 'delete'])->middleware('admin')->name('articles.delete');
-Route::get('/admin/articles/{article}/edit', [ArticleController::class, 'edit'])->middleware('admin')->name('articles.edit');
-Route::put('/admin/articles/{article}/update', [ArticleController::class, 'update'])->middleware('admin')->name('articles.update');
+Route::prefix('admin')->group(function(){
+    Route::get('/articles', [ArticleController::class, 'index'])->middleware('admin')->name('articles.index');
+    Route::get('/articles/create', [ArticleController::class, 'create'])->middleware('admin')->name('articles.create');
+    Route::post('/articles/store', [ArticleController::class, 'store'])->middleware('admin')->name('articles.store');
+    Route::delete('/articles/{article}/delete}', [ArticleController::class, 'delete'])->middleware('admin')->name('articles.delete');
+    Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->middleware('admin')->name('articles.edit');
+    Route::put('/articles/{article}/update', [ArticleController::class, 'update'])->middleware('admin')->name('articles.update');
+    });
+
