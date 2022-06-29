e8266eec13206d839fd6c41a9ed67e0e70d70322
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2022-06-29 23:44:58 +0200

    Livewire/Implementation du counter

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,6 +10,7 @@
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
     <title>Hello, world!</title>
+    @livewireStyles
   </head>
   <body>
     @include('incs.navbar')
@@ -23,5 +24,6 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script src = "https://kit.fontawesome.com/907683f3ba.js" ></script>
+    @livewireScripts
   </body>
 </html>
