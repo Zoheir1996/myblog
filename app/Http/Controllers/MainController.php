65e17b0956e6dbe8eaa66479b3a428918f382029
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2022-07-05 15:59:58 +0200

    Implementation de livewire pour filtrer les categories

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
+
 
 class MainController extends Controller
 {
--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -3,41 +3,9 @@
 @section('content')
 <div class="jumbotron">
     <h1 class = "display-3 text-center">Articles</h1>
-   <div class="row">
-       <div class="col-10">
-        <div class="articles row justify-content-center">
-            @foreach ($articles as $article)
-            <div class = "col-md-6 ">
-                <div class="card my-3">
-                    <div class="card-body">
-                    <h5 class = "card-title">{{$article -> title }}</h5>
-                    <span class="badge bg-dark">{{$article->category->label}}</span>
-                    <p class = "card-text">{{ $article -> subtitle}}</p>
-                    <a href="{{  route('article',$article->slug) }}" class = "btn btn-primary">
-                        Lire la suite
-                        <i class="fas fa-arrow-right"></i>
-                    </a>
-                    </div>
-                    </div>
-                </div>
-            @endforeach
-       </div>
-   </div>
-    <div class="col-2 pt-3">
-        @foreach ($categories as $category)
-        <div class="form-group">
-            <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input"/>
-                <label class="custom-control-label">
-                    <i class="fas fa--{{$category->icon}}"></i>
-                    {{$category->label}}
-                </label>
-            </div>
-        </div>
-        @endforeach
-    </div>
-    <div class="d-flex justify-content-center mt-5">
+    @livewire('filters',['categories' =>$categories])
+    {{-- <div class="d-flex justify-content-center mt-5">
         {{$articles ->links('vendor.pagination.custom')}}
-    </div>
+    </div> --}}
 </div>
 @endsection 
